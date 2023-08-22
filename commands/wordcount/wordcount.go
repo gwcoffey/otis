d69cb3e63f90edc0b9973d3025da3346a54b259f
Geoff Coffey <gwcoffey@me.com>
2023-08-21 23:08:10 -0700

    add ms package for working with manuscript dir, re-implement wordcount using this

--- commands/wordcount/wordcount.go
+++ commands/wordcount/wordcount.go
@@ -1,16 +1,14 @@
 package wordcount
 
 import (
+	"errors"
 	"flag"
 	"fmt"
-	"golang.org/x/text/cases"
 	"golang.org/x/text/language"
 	"golang.org/x/text/message"
 	"gwcoffey/otis/shared/cli"
+	"gwcoffey/otis/shared/ms"
 	"os"
-	"path/filepath"
-	"regexp"
-	"strconv"
 	"strings"
 	"unicode/utf8"
 )
@@ -18,67 +16,23 @@ import (
 const maxWidth = 40
 const indentSize = "  "
 
-type node struct {
-	Path     string
-	Children []node
-	Words    int
-}
-
-func wordCount(s string) int {
-	return len(strings.Fields(s))
-}
-
-func readDir(path string) node {
-	entries, err := os.ReadDir(path)
+func sceneWordCount(scene ms.Scene) int {
+	text, err := scene.Text()
 	if err != nil {
 		panic(err)
 	}
-
-	var children []node
-	totalWords := 0
-
-	for _, entry := range entries {
-		subPath := filepath.Join(path, entry.Name())
-
-		if entry.IsDir() {
-			thisNode := readDir(subPath)
-			totalWords += thisNode.Words
-			children = append(children, thisNode)
-		} else {
-			if strings.HasSuffix(entry.Name(), ".md") {
-				data, err := os.ReadFile(subPath)
-				if err != nil {
-					panic(err)
-				}
-				words := wordCount(string(data))
-				totalWords += words
-				children = append(children, node{Path: subPath, Children: []node{}, Words: words})
-			}
-		}
-	}
-
-	return node{Path: path, Children: children, Words: totalWords}
+	return len(strings.Fields(*text))
 }
 
-func prettify(str string) string {
-	clean := str
-	if filepath.Ext(str) == ".md" {
-		re := regexp.MustCompile(`^(\d+)-(.*).md$`)
-		matches := re.FindStringSubmatch(str)
-		if len(matches) == 3 {
-			num, err := strconv.Atoi(matches[1])
-			if err != nil {
-				panic("no scene number on " + str)
-			}
-
-			clean = fmt.Sprintf("%02d. %s", num, matches[2])
-		}
+func dirWordCount(dir ms.Dir) int {
+	total := 0
+	for _, scene := range dir.Scenes() {
+		total += sceneWordCount(scene)
 	}
-	words := strings.Split(clean, "-")
-	if len(words) > 0 {
-		words[0] = cases.Title(language.English).String(words[0])
+	for _, subdir := range dir.SubDirs() {
+		total += dirWordCount(subdir)
 	}
-	return strings.Join(words, " ")
+	return total
 }
 
 func truncate(str string) string {
@@ -86,22 +40,61 @@ func truncate(str string) string {
 	if utf8.RuneCountInString(result) > maxWidth {
 		result = result[0:maxWidth-1] + "…"
 	}
-
 	return result
 }
 
-func printDir(out *message.Printer, aNode *node, indent string) {
+func printDir(dir ms.Dir, indent string) {
+	printLine(truncate(indent+dir.Name()), dirWordCount(dir), true)
+	for _, subdir := range dir.SubDirs() {
+		printDir(subdir, indent+indentSize)
+	}
+	for _, scene := range dir.Scenes() {
+		printScene(scene, indent+indentSize)
+	}
+}
 
-	name := truncate(indent + prettify(filepath.Base(aNode.Path)))
+func printScene(scene ms.Scene, indent string) {
+	printLine(truncate(indent+scene.Name()), sceneWordCount(scene), false)
+}
+
+func printLine(label string, count int, emphasize bool) {
+	out := message.NewPrinter(language.English)
 	format := fmt.Sprintf("%%-%d.%ds : %%7d\n", maxWidth, maxWidth)
-	_, err := out.Printf(format, name, aNode.Words)
+	if emphasize {
+		format = fmt.Sprintf("\033[94m%s\033[0m", format)
+	}
+	_, err := out.Printf(format, label, count)
 	if err != nil {
 		panic(err)
 	}
+}
 
-	for _, content := range aNode.Children {
-		printDir(out, &content, indent+indentSize)
+func readParams(fs *flag.FlagSet) (*string, error) {
+	if fs.NArg() == 0 {
+		return nil, nil
+	} else if fs.NArg() == 1 {
+		result := fs.Arg(0)
+		return &result, nil
+	} else {
+		return nil, errors.New("unexpected parameters")
 	}
+}
+
+func findMsRoot(path *string) ms.Dir {
+	root := ms.LoadMs()
+
+	// if a path is specified dig for it
+	if path != nil {
+		for _, name := range strings.Split(*path, "/") {
+			newRoot := root.SubDir(name)
+			if newRoot == nil {
+				panic(fmt.Sprintf("no such manuscript directory: %s", path))
+			}
+			root = *newRoot
+		}
+	}
+
+	return root
 }
 
 func WordCount(args []string) {
@@ -109,19 +102,12 @@ func WordCount(args []string) {
 	fs.Usage = cli.UsageFn("wordcount [path]")
 	cli.MustParse(fs, args)
 
-	// use the full manuscript by default...
-	rootPath := "manuscript/"
-
-	// ...unless a path is specified
-	if fs.NArg() == 1 {
-		rootPath = fs.Arg(0)
+	path, err := readParams(fs)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
-	out := message.NewPrinter(language.English)
-
-	rootNode := readDir(rootPath)
-
-	_, _ = out.Println()
-	printDir(out, &rootNode, "")
-	_, _ = out.Println()
+	root := findMsRoot(path)
+	printDir(root, "")
 }
---
+++ shared/ms/ms.go
@@ -0,0 +1,237 @@
+// Package ms provides primitives for working with the manuscript content
+package ms
+
+import (
+	"errors"
+	"fmt"
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+// Type node represents a filesystem node within the `manuscript` directory.
+// It conforms to both Dir and Scene interfaces
+type node struct {
+	// the path to the filesystem object
+	Path string
+
+	// the parent node
+	ParentNode *node
+
+	// all child nodes
+	ChildNodes []*node
+
+	// the node type
+	Type NodeType
+
+	// the content of a file this node represents (if loaded)
+	Content *[]byte
+}
+
+type NodeType int
+
+const (
+	NodeTypeDir NodeType = iota
+	NodeTypeScene
+)
+
+const markdownExt = ".md"
+const kebabSeparator = "-"
+
+type INode interface {
+	// Parent Function returns the parent directory
+	Parent() Dir
+
+	// Filename returns the name (including extension) of the filesystem object
+	// that underlies this node
+	Filename() string
+
+	// Name returns the pretty name by converting snake-case to camelcase
+	Name() string
+}
+
+type Dir interface {
+	INode
+
+	// SubDirs function returns the direct child directories
+	SubDirs() []Dir
+
+	// SubDir returns the direct child directory with the given name
+	SubDir(name string) *Dir
+
+	// Scenes returns the scenes within this directory
+	Scenes() []Scene
+}
+
+type Scene interface {
+	INode
+
+	// Text returns the textual content (in raw Markdown) of the scene
+	// Calling this causes the scene to be loaded if it isn't already, and the
+	// error returned is any error from the reader
+	Text() (*string, error)
+
+	// Number returns the scene number, which is the prefix of the filename
+	Number() int
+}
+
+func readDir(path string) *node {
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		panic(err)
+	}
+
+	var children []*node
+	var thisNode = node{Path: path, ChildNodes: children, Type: NodeTypeDir}
+
+	for _, entry := range entries {
+		subPath := filepath.Join(path, entry.Name())
+		if entry.IsDir() {
+			nextNode := readDir(subPath)
+			children = append(children, nextNode)
+		} else if filepath.Ext(subPath) == markdownExt {
+			nextNode := node{Path: subPath, ParentNode: &thisNode, ChildNodes: []*node{}, Type: NodeTypeScene}
+			children = append(children, &nextNode)
+		}
+	}
+
+	thisNode.ChildNodes = children
+
+	return &thisNode
+}
+
+func (n *node) DumpToString() string {
+	sb := strings.Builder{}
+	switch n.Type {
+	case NodeTypeDir:
+		sb.WriteString("node(dir)@")
+	case NodeTypeScene:
+		sb.WriteString("node(scene)@")
+	default:
+		panic(fmt.Sprintf("unexpected node type: %s", n.Type))
+	}
+	sb.WriteString(n.Filename())
+	if len(n.ChildNodes) > 0 {
+		sb.WriteString("{")
+		for idx, child := range n.ChildNodes {
+			if idx > 0 {
+				sb.WriteRune(',')
+			}
+			sb.WriteString(child.DumpToString())
+		}
+		sb.WriteString("}")
+	}
+
+	return sb.String()
+}
+
+// BEGIN INode interface
+
+func (n *node) Parent() Dir {
+	return n.ParentNode
+}
+
+func (n *node) Filename() string {
+	return filepath.Base(n.Path)
+}
+
+func (n *node) Name() string {
+	switch n.Type {
+	case NodeTypeDir:
+		return kebabToSentence(n.Filename())
+	case NodeTypeScene:
+		namePart := n.Filename()
+		re := regexp.MustCompile(`^\d+-?(.*).md`)
+		matches := re.FindStringSubmatch(namePart)
+		if len(matches) == 2 {
+			namePart = matches[1]
+		}
+		return fmt.Sprintf("%02d. %s", n.Number(), kebabToSentence(namePart))
+	}
+
+	panic(errors.New(fmt.Sprintf("No such type: %s", n.Type)))
+}
+
+func kebabToSentence(str string) string {
+	words := strings.Split(str, kebabSeparator)
+	words[0] = cases.Title(language.English).String(words[0])
+	return strings.Join(words, " ")
+}
+
+// BEGIN Dir interface
+
+func (n *node) Scenes() []Scene {
+	var result []Scene
+	for _, child := range n.ChildNodes {
+		if child.Type == NodeTypeScene {
+			result = append(result, child)
+		}
+	}
+	return result
+}
+
+func (n *node) SubDirs() []Dir {
+	var result []Dir
+	for _, child := range n.ChildNodes {
+		if child.Type == NodeTypeDir {
+			result = append(result, child)
+		}
+	}
+	return result
+}
+
+func (n *node) SubDir(name string) *Dir {
+	for _, dir := range n.SubDirs() {
+		if dir.Filename() == name {
+			return &dir
+		}
+	}
+
+	return nil
+}
+
+// BEGIN Scene interface
+
+func (n *node) load() error {
+	if n.Content == nil {
+		bytes, err := os.ReadFile(n.Path)
+		if err != nil {
+			return err
+		}
+		n.Content = &bytes
+	}
+	return nil
+}
+
+func (n *node) Text() (*string, error) {
+	err := n.load()
+	if err != nil {
+		return nil, err
+	}
+	text := string(*n.Content)
+	return &text, nil
+}
+
+func (n *node) Number() int {
+	re := regexp.MustCompile(`^\d+`)
+	matches := re.FindStringSubmatch(n.Filename())
+	if len(matches) == 1 {
+		num, err := strconv.Atoi(matches[0])
+		if err != nil {
+			panic(err)
+		}
+		return num
+	}
+	return 0
+}
+
+// BEGIN public interface
+
+func LoadMs() Dir {
+	x := readDir("manuscript/")
+	return x
+}
